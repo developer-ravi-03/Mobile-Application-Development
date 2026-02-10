95dd557ed92a36528d8809d06f7d436c384b6865
Ravi Kumar <ravibseb96@gmail.com>
2026-02-10 11:45:43 +0530

    Send Intent Done

--- Practical-04/app/src/main/java/com/example/practical_04/MainActivity.java
+++ Practical-04/app/src/main/java/com/example/practical_04/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,10 +30,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //send Text
+    private EditText et;
     public void Asend(View view ){
+        //catch text
+        et=findViewById(R.id.EditText1);
+        String userInput=et.getText().toString();
+
+        //send Text
         Intent sendIntent=new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT,"Hello");
+        sendIntent.putExtra(Intent.EXTRA_TEXT,userInput);
         sendIntent.setType("text/plain");
         Intent shareIntent=Intent.createChooser(sendIntent,null);
         startActivity(shareIntent);
