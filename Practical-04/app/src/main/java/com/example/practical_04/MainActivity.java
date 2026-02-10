3ba52470fdf15729d27dfbc0404eed790e04dc04
Ravi Kumar <ravibseb96@gmail.com>
2026-02-10 12:02:28 +0530

    Send Fixed Image Done

--- Practical-04/app/src/main/java/com/example/practical_04/MainActivity.java
+++ Practical-04/app/src/main/java/com/example/practical_04/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.practical_04;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     }
     private Uri imageUri;
 //    private Intent intent;
-
     public void Click(View view)
     {
         Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://chatgpt.com/"));
@@ -35,7 +38,10 @@ public class MainActivity extends AppCompatActivity {
         //catch text
         et=findViewById(R.id.EditText1);
         String userInput=et.getText().toString();
-
+        if(userInput.isEmpty()){
+            Toast.makeText(this, "Please Enter Message", Toast.LENGTH_SHORT).show();
+            return;
+        }
         //send Text
         Intent sendIntent=new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
@@ -43,5 +49,38 @@ public class MainActivity extends AppCompatActivity {
         sendIntent.setType("text/plain");
         Intent shareIntent=Intent.createChooser(sendIntent,null);
         startActivity(shareIntent);
+    }
+
+    //Send Image Logic
+    public void image(View view)
+    {
+        BitmapDrawable drawable = (BitmapDrawable) ContextCompat.getDrawable(this, R.drawable.profilecard);
+
+        if (drawable == null) {
+            Toast.makeText(this, "Image not found", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Bitmap bitmap = drawable.getBitmap();
+
+        String path = MediaStore.Images.Media.insertImage(
+                getContentResolver(),
+                bitmap,
+                "SharedImage",
+                null
+        );
+
+        if (path == null) {
+            Toast.makeText(this, "Image not shared", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Uri imageUri = Uri.parse(path);
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_STREAM, imageUri);
+
+        startActivity(Intent.createChooser(intent, "Share Image"));
     }
 }
